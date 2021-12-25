58a919a51907bf13ac58a5a8f0ca7eee146af9d1
Farzin <43478550+inPhraZ@users.noreply.github.com>
2021-12-25 21:23:17 +0330

    C keywords

---
+++ keywords.h
@@ -0,0 +1,56 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  keywords.h
+ *       Version:   1.0
+ *       Created:   12/25/2021 09:21:10 PM
+ *       Author:    Farzin
+ *
+ * =====================================================================================
+ */
+
+#ifndef     __FARCN_KEYWORDS_H_
+#define     __FARCN_KEYWORDS_H_
+
+// C keywords
+#define     C_AUTO      "auto"
+#define     C_BREAK     "break"
+#define     C_CASE      "case"
+#define     C_CHAR      "char"
+#define     C_CONST     "const"
+#define     C_CONTINUE  "continue"
+#define     C_DEFAULT   "default"
+#define     C_DO        "do"
+#define     C_DOUBLE    "double"
+#define     C_ELSE      "else"
+#define     C_ENUM      "enum"
+#define     C_EXTERN    "extern"
+#define     C_FLOAT     "float"
+#define     C_FOR       "for"
+#define     C_GOTO      "goto"
+#define     C_IF        "if"
+#define     C_INLINE    "inline"
+#define     C_INT       "int"
+#define     C_LONG      "long"
+#define     C_REGISTER  "register"
+#define     C_RESTRICT  "restrict"
+#define     C_RETURN    "return"
+#define     C_SHORT     "short"
+#define     C_SIGNED    "signed"
+#define     C_SIZEOF    "sizeof"
+#define     C_STATIC    "static"
+#define     C_STRUCT    "struct"
+#define     C_SWITCH    "switch"
+#define     C_TYPEDEF   "typedef"
+#define     C_UNION     "union"
+#define     C_UNSIGNED  "unsigned"
+#define     C_VOID      "void"
+#define     C_VOLATILE  "volatile"
+#define     C_WHILE     "while"
+
+// PreProcessors
+#define     C_DEFINE    "define"
+#define     C_ENDIF     "endif"
+#define     C_INCLUDE   "include"
+
+#endif      /*  __FARCN_KEYWORDS_H_ */
