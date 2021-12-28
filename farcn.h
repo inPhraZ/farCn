9b89e871c4da54c7ccf5cb77c4d408a4e7d84067
Farzin <43478550+inPhraZ@users.noreply.github.com>
2021-12-28 20:04:47 +0330

    yyin, yyout

--- farcn.h
+++ farcn.h
@@ -10,13 +10,20 @@
 #ifndef     __FARCN_LANG_H_
 #define     __FARCN_LANG_H_
 
-#define     FARCN_REPLACE(C_Token)  \
-    do { fprintf(yyout, C_Token); } while(0)
-
 #if defined(__linux__)
 #include <linux/limits.h>
 #else
 #define NAME_MAX        255
 #endif
+
+#define     FARCN_REPLACE(C_Token)  \
+    do { fprintf(yyout, C_Token); } while(0)
+
+// lex input fileptr
+extern FILE *yyin;
+// lex output fileptr
+extern FILE *yyout;
+
+extern int farcn_main(int argc, char **argv);
 
 #endif      /*  __FARCN_LAN_H_ */
