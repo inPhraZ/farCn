5ac1eb8243737c7ffc3168a20103a7b01001622d
Farzin <43478550+inPhraZ@users.noreply.github.com>
2021-12-29 17:06:01 +0330

    validate extension of source files

--- farcn.c
+++ farcn.c
@@ -7,9 +7,43 @@
  * =====================================================================================
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <regex.h>
+
 #include "farcn.h"
+
+static const char *farcn_reg_valid_ext = ".*[.](c|h|farcn|farhn)$";
+static regex_t     farcn_reg_t;
+
+// check if a filename extension is valid
+static int is_valid_ext(const char *str)
+{
+    if (regexec(&farcn_reg_t, str, 0, NULL, 0))
+        return 0;
+    return 1;
+}
 
 int farcn_main(int argc, char **argv)
 {
-    return 0;
+    int i;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s  [FILE(s)]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if (regcomp(&farcn_reg_t, farcn_reg_valid_ext, REG_EXTENDED)) {
+        fprintf(stderr, "Inner error\n");
+        return EXIT_FAILURE;
+    }
+
+    for (i = 1; i < argc; ++i) {
+        if (is_valid_ext(argv[i])) {
+            printf("%s has valid extension\n", argv[i]);
+            continue;
+        }
+        fprintf(stderr, "%s: file format not recognized\n", argv[i]);
+    }
+    return EXIT_SUCCESS;
 }
